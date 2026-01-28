75cec94a8898d96619c798ac8d16b544e744e913
shanto <hasibul.hossain.dev@gmail.com>
2026-01-28 12:06:47 +0600

    Add better-auth integration and update Prisma schema
    - Introduced better-auth for authentication handling.
    - Updated package.json and package-lock.json to include better-auth dependency.
    - Modified Prisma configuration and migration files to reflect new user and session models.
    - Created new models for Seller, Meal, Category, and Verification in Prisma schema.
    - Adjusted TypeScript configuration for improved module resolution.
    - Updated server entry point in package.json.

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,8 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: "prisma/schema",
+  
   migrations: {
     path: "prisma/migrations",
   },
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,8 +1,15 @@
+import { toNodeHandler } from "better-auth/node";
 import express from "express"
-
+import {auth} from '../utils/auth.js'
 const app = express()
 app.use(express.json())
 
+app.use((req,res,next)=>{
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
+    next()
+})
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
 
 app.get('/',(req,res) => {
     res.send("The server is working")
